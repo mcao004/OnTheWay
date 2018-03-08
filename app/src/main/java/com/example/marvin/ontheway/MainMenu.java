b4c4afe95f5dce76b045c6a204f3db2640973f36
mcao004 <mcao004@ucr.edu>
2018-03-07 19:58:21 -0800

    Added top bar without implementation

--- app/src/main/java/com/example/marvin/ontheway/MainMenu.java
+++ app/src/main/java/com/example/marvin/ontheway/MainMenu.java
@@ -1,8 +1,14 @@
 package com.example.marvin.ontheway;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -17,8 +23,47 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        // TextView tv = (TextView) findViewById(R.id.sample_text);
+        // tv.setText(stringFromJNI());
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mMenuInflater = getMenuInflater();
+        mMenuInflater.inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_map){
+            Toast.makeText(MainMenu.this,
+                    "You have clicked on map action menu",
+                    Toast.LENGTH_SHORT)
+                    .show();
+//            Intent mapActivityIntent = new Intent(this, MapActivity.class);
+//            startActivity(mapActivityIntent);
+//            finish();
+        }
+        if(item.getItemId() == R.id.action_setting){
+            Toast.makeText(MainMenu.this,
+                    "You have clicked on setting action menu",
+                    Toast.LENGTH_SHORT)
+                    .show();
+            // Intent settingsActivityIntent = new Intent(this, SettingActivity.class);
+            // startActivity(settingsActivityIntent);
+            // finish();
+        }
+        if(item.getItemId() == R.id.action_about_us){
+            Toast.makeText(MainMenu.this,
+                    "You have clicked on about us action menu",
+                    Toast.LENGTH_SHORT)
+                    .show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     /**
